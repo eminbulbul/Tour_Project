9cbbc2cbde2b45268193f592b145dd8111ec3fa6
m_emin <mehmeteminbul@gmail.com>
2022-03-23 22:03:24 +0300

    session 1 ended

---
+++ solution/src/App.js
@@ -0,0 +1,13 @@
+import "./App.scss";
+
+
+const App = () => {
+  return(
+    <div className="App">
+      <h1>Welcome React</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, accusantium.</p>
+    </div>
+  )
+}
+
+export default App;
